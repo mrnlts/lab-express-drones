442a20199c333abe9801aede2523f9ab026633b2
marina <marinaaltesmagret@gmail.com>
2021-04-25 09:44:45 +0200

    Iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -6,22 +6,18 @@ const router = express.Router();
 
 router.get('/drones', (req, res) => {
   Drone.find({})
-    .then((drones)=> {
-      console.log(drones);
-      res.render('drones/list', {drones});
-    })
+    .then((drones)=> res.render('drones/list', {drones}))
     .catch((err) => console.log('Error retrieving drones from DB: ', err));
 });
 
-// router.get('/drones/create', (req, res, next) => {
-//   // Iteration #3: Add a new drone
-//   // ... your code here
-// });
+router.get('/drones/create', (req, res, next) => res.render('drones/create-form'));
 
-// router.post('/drones/create', (req, res, next) => {
-//   // Iteration #3: Add a new drone
-//   // ... your code here
-// });
+router.post('/drones/create', (req, res, next) => {
+  const {name, propellers, maxSpeed} = req.body;
+  Drone.create({name, propellers, maxSpeed})
+    .then(()=> res.redirect('/drones'))
+    .catch(()=> res.render('drones/create-form'))
+});
 
 // router.get('/drones/:id/edit', (req, res, next) => {
 //   // Iteration #4: Update the drone
